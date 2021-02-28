bcbef830659981bb8a35cd85e0efc8c65941b16a
CPJ31415 <383476064@qq.com>
2021-02-28 22:33:59 +0800

    2-9 增加自定义配置示例

--- src/main/java/com/huajuan/wiki/controller/TestController.java
+++ src/main/java/com/huajuan/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.huajuan.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,13 @@ public class TestController {
     /*
      * GET, POST, PUT, DELETE
      */
+    @Value("${test.hello: Test")
+    private String testHello;
 
     @GetMapping("/hello")
     //GetPosting 405
     public String hello() {
-        return "hello world";
+        return "hello world" + testHello;
     }
 
     @PostMapping("/hello/post")
