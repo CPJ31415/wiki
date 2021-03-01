39aae04757be01b9a35ffccd6df44d8c23bc7c9e
CPJ31415 <383476064@qq.com>
2021-03-01 19:40:43 +0800

    3-5 增加demo表查询

---
+++ src/main/java/com/huajuan/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.huajuan.wiki.service;
+
+import com.huajuan.wiki.domain.Demo;
+import com.huajuan.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
