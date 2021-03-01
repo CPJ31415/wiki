4973e0c5b0f0022619841c1b20998e83e5e132ad
CPJ31415 <383476064@qq.com>
2021-03-01 19:57:43 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/huajuan/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.huajuan.wiki.service;
+
+import com.huajuan.wiki.domain.Ebook;
+import com.huajuan.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
