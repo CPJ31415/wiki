5726f3760482afcd530d4829931f3a3b272a3879
CPJ31415 <383476064@qq.com>
2021-03-03 21:31:38 +0800

    5-4 列表查询接口支持动态SQL name参数有值时按name查，没有时查全部

--- src/main/java/com/huajuan/wiki/service/EbookService.java
+++ src/main/java/com/huajuan/wiki/service/EbookService.java
@@ -7,6 +7,7 @@ import com.huajuan.wiki.req.EbookReq;
 import com.huajuan.wiki.resp.EbookResp;
 import com.huajuan.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -23,7 +24,9 @@ public class EbookService {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
 
-        criteria.andNameLike("%" + req.getName() + "%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
         //列表复制
